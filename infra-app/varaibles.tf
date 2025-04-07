fd81a3a397a1599f94bf99d3f64379cda1cfaf08
dishant dudhat <dobariyadishant@gmail.com>
2025-04-07 12:14:53 +0530

    Change variable type

--- infra-app/varaibles.tf
+++ infra-app/varaibles.tf
@@ -1,24 +1,24 @@
 variable "env" {
   description = "This is the env for my infra"
-  type = "string"
+  type = string
 }
 
 variable "bucket_name" {
   description = "This is the bucket name"
-  type = "string"
+  type = string
 }
 
 variable "instance_count" {
   description = "This is the count of intance"
-  type = "number"
+  type = number
 }
 
 variable "instance_type" {
   description = "This is instance type"
-  type = "string"
+  type = string
 }
 
 variable "ami_id" {
   description = "This is ami_id"
-  type = "string"
+  type = string
 }
