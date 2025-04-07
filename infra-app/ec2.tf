cf60d0b29ceb4a6e1e4dcf90d566d5ef148fac97
dishant dudhat <dobariyadishant@gmail.com>
2025-04-07 12:01:56 +0530

    Added ec2

---
+++ infra-app/ec2.tf
@@ -0,0 +1,77 @@
+resource "aws_key_pair" "terra_infra_key" {
+  key_name = "${var.env}-infra-app-ley"
+  public_key = file("terra-infra-app.pub")
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "Defualt vpc"
+  }
+}
+
+resource "aws_security_group" "infra_app_security_group" {
+    name = "${var.env}-infra-app-security-group"
+    description = "This security group created using TF"
+    vpc_id = aws_default_vpc.default_vpc.vpc_id
+    
+    ingress {
+        to_port = 22
+        from_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "SSH open"
+    }
+
+    ingress {
+        to_port = 80
+        from_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTP open"
+    }
+
+    ingress {
+        to_port = 443
+        from_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTPS open"
+    }
+
+    egress {
+        to_port = 0
+        from_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "open for all"
+    }
+
+    tags = {
+      Name = "${var.env}-infra-app-sg"
+      
+    }
+  
+}
+
+resource "aws_instance" "infra_app_instance" {
+  count = var.instance_count
+  key_name = aws_key_pair.terra_infra_key.key_name
+  security_groups = [aws_security_group.infra_app_security_group.name]
+  instance_type = var.instance_type
+  ami = var.ami_id
+
+  root_block_device {
+    volume_size = var.env == "prod" ? 20 : 10 
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.env}-infra-app-instance"
+    Environment = var.env
+  }
+
+}
--- infra-app/varaibles.tf
+++ infra-app/varaibles.tf
@@ -7,3 +7,18 @@ variable "bucket_name" {
   description = "This is the bucket name"
   type = "string"
 }
+
+variable "instance_count" {
+  description = "This is the count of intance"
+  type = "number"
+}
+
+variable "instance_type" {
+  description = "This is instance type"
+  type = "string"
+}
+
+variable "ami_id" {
+  description = "This is ami_id"
+  type = "string"
+}
