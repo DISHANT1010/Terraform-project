63f12f1f196404489fafc9f928382b403c5feb99
dishant dudhat <dobariyadishant@gmail.com>
2025-04-07 12:27:45 +0530

    Added another modules

--- main.tf
+++ main.tf
@@ -6,3 +6,21 @@ module "dev-infra" {
   instance_type = "t2.micro"
   ami_id = "ami-04f167a56786e4b09"
 }
+
+module "prd-infra" {
+  source = "./infra-app"
+  env = "prd"
+  bucket_name = "dishant-infra-app-bucket"
+  instance_count = 2
+  instance_type = "t2.micro"
+  ami_id = "ami-04f167a56786e4b09"
+}
+
+module "stg-infra" {
+  source = "./infra-app"
+  env = "stg"
+  bucket_name = "ishant-infra-app-bucket"
+  instance_count = 1
+  instance_type = "t2.micro"
+  ami_id = "ami-04f167a56786e4b09"
+}
