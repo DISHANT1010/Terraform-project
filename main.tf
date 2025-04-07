a19b4a53efb97ddd54c692e46b0f95dd56efcf41
dishant dudhat <dobariyadishant@gmail.com>
2025-04-07 12:12:48 +0530

    Added dev modules

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "aws_s3_bucket" "infra_app_bucket" {
-  bucket = "infra-app-bucket"
-
-  tags = {
-    Name = "infra-app-bucket"
-  }
+module "dev-infra" {
+  source = "./infra-app"
+  env = "dev"
+  bucket_name = "dishant-infra-app-bucket"
+  instance_count = 1
+  instance_type = "t2.micro"
+  ami_id = "ami-04f167a56786e4b09"
 }
